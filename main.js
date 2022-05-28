3aed3340eb7e83d878f68cc7083e583f175d10d4
SnehaSinha123 <78296466+SnehaSinha123@users.noreply.github.com>
2022-05-28 22:20:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function addUser(){
+    window.location="object_page_bedroom.html";
+}
+
+function addUser2(){
+    window.location="object_page_piano.html";
+}
+
+function addUser3(){
+    window.location="object_page_kitchen.html";
+}
+
+function addUser4(){
+    window.location="object_page_study_room.html";
+}
+
+function addUser5(){
+    window.location="object_page_drawing_room.html";
+}
